b0b8830cf669142ee3279e401ad77cfe869d098c
caden-deutscher <caden.deutscher@outlook.com>
2023-12-02 22:13:03 -0800

    Basic Cosmetic Changes

--- lib/pages/create_page.dart
+++ lib/pages/create_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:chuckler/route_generator.dart';
 import 'package:chuckler/AppNavBar.dart';
 import 'package:chuckler/globalvars.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
 class CreatePage extends StatelessWidget {
   @override
@@ -21,28 +22,53 @@ class CreateForm extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
+        //Heading
+        Expanded(
+          flex: 5,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.black
+            )
+
+          )
+        ),
         //Post Promt
         Expanded(
-          flex: 25,
+          flex: 20,
           child: Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 Expanded(flex: 1, child: Container()),
                 Expanded(
-                    flex: 2,
+                    flex: 20,
                     child: FractionallySizedBox(
                         heightFactor: 0.5,
                         child: Container(
-                          child:Center( child: Text("The Promt will appear here\nCheck this",
-                              style: TextStyle(color: Colors.white))),
                           decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(20.0),
                               color: Colors.black),
+                          child: Center(
+                              child: AutoSizeText(
+                                  "Marvel should replace Captain America with _____",
+                                  style: TextStyle(
+                                    fontSize: 40,
+                                      color: Colors.white,
+                                      fontFamily: 'OpenSans',
+                                      fontWeight: FontWeight.w700),
+                                  textAlign: TextAlign.center
+                              )
+                          ),
                         ))),
                 Expanded(flex: 1, child: Container()),
               ]),
         ),
+        Expanded(
+            flex: 10,
+            child: Container(
+                alignment: Alignment.center,
+                child: Text("24:00:00 remain",
+                    style: TextStyle(fontFamily: 'Livvic', fontWeight: FontWeight.w600)))),
         //Text Box Container
         Expanded(
           flex: 25,
@@ -61,23 +87,26 @@ class CreateForm extends StatelessWidget {
             child: Container(
                 color: Colors.black,
                 alignment: Alignment.center,
-                child: Expanded(
                     //Row For the Post Button
                     child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
                       ElevatedButton(
-                        child: Text("POST", style: TextStyle(fontSize: 30)),
+                        child: Text("Post", style: TextStyle(fontSize: 20, fontFamily: 'Livvic', fontWeight: FontWeight.w600)),
                         onPressed: () {
                           print("Post Clicked");
                         },
                         style: ButtonStyle(
                           backgroundColor:
                               MaterialStateProperty.all<Color>(Colors.amber),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(50))),
                         ),
                       ),
-                    ])))),
+                    ]))),
       ],
     );
   }
