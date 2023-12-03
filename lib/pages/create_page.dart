0017ccff35b802828c31732341a414bc958bd2f7
caden-deutscher <caden.deutscher@outlook.com>
2023-12-03 13:07:10 -0800

    Files/Firebase Fix
    Fixed firebase initiation issue and added files for create page for better readability.

--- lib/pages/create_page.dart
+++ lib/pages/create_page.dart
@@ -4,6 +4,9 @@ import 'package:chuckler/AppNavBar.dart';
 import 'package:chuckler/globalvars.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:chuckler/AppHeaderMain.dart';
+import 'package:chuckler/CreatePageContent.dart';
+
+
 
 class CreatePage extends StatelessWidget {
   @override
@@ -28,81 +31,11 @@ class CreateForm extends StatelessWidget {
           flex: 5,
           child: MainHeaderBar()
         ),
-        //Post Promt
         Expanded(
-          flex: 20,
-          child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Expanded(flex: 1, child: Container()),
-                Expanded(
-                    flex: 20,
-                    child: FractionallySizedBox(
-                        heightFactor: 0.5,
-                        child: Container(
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20.0),
-                              color: Colors.black),
-                          child: Center(
-                              child: AutoSizeText(
-                                  "Marvel should replace Captain America with _____",
-                                  style: TextStyle(
-                                    fontSize: 40,
-                                      color: Colors.white,
-                                      fontFamily: 'OpenSans',
-                                      fontWeight: FontWeight.w700),
-                                  textAlign: TextAlign.center
-                              )
-                          ),
-                        ))),
-                Expanded(flex: 1, child: Container()),
-              ]),
-        ),
-        Expanded(
-            flex: 10,
-            child: Container(
-                alignment: Alignment.center,
-                child: Text("24:00:00 Remaining",
-                    style: TextStyle(fontFamily: 'Livvic', fontWeight: FontWeight.w600)))),
-        //Text Box Container
-        Expanded(
-          flex: 25,
-          child: Container(
-            decoration: BoxDecoration(border: Border.all(width: 10)),
-            child: TextField(
-              cursorColor: Colors.black,
-              decoration: InputDecoration(
-                  hintText: "Answer Prompt Here", border: InputBorder.none),
-            ),
-          ),
-        ),
-        //Button Container
-        Expanded(
-            flex: 4,
-            child: Container(
-                color: Colors.black,
-                alignment: Alignment.center,
-                    //Row For the Post Button
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                      ElevatedButton(
-                        child: Text("Post", style: TextStyle(fontSize: 20, fontFamily: 'Livvic', fontWeight: FontWeight.w600)),
-                        onPressed: () {
-                          print("Post Clicked");
-                        },
-                        style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all<Color>(Colors.amber),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(50))),
-                        ),
-                      ),
-                    ]))),
+          flex:60,
+          child: CreatePageContent()
+        )
+
       ],
     );
   }
