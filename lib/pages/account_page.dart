d171fe3dcc68282559ff7224b72e2bad0607102b
Devam Patel <devam29@gmail.com>
2024-04-27 20:15:03 -0700

    added additional confirmation to logout button from accounts page

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -134,9 +134,52 @@ class _AccountPageState extends State<AccountPage> {
                   height: 10,
                 ),
                 ElevatedButton.icon(
-                  onPressed: () async {
-                    await _auth.signOut();
-                    Navigator.pushReplacementNamed(context, '/login');
+                  onPressed: () {
+                    // confirm the user wants to log out
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return Center(
+                          child: AlertDialog(
+                            shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20))),
+                            backgroundColor: Color.fromARGB(255, 20, 20, 20),
+                            title: const Text('Confirmation'),
+                            titleTextStyle: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFFffd230),
+                                fontSize: 22),
+                            content: Text('Are you sure you want to sign out?'),
+                            contentTextStyle: const TextStyle(
+                                fontSize: 14, color: Colors.white),
+                            actions: [
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    foregroundColor: Colors.black,
+                                    backgroundColor: const Color(0xFFffd230)),
+                                child: Text('Cancel'),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  foregroundColor: Colors.black,
+                                  backgroundColor: Colors.white,
+                                ),
+                                child: Text('Yes'),
+                                onPressed: () async {
+                                  await _auth.signOut();
+                                  Navigator.pushReplacementNamed(
+                                      context, '/login');
+                                },
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    );
                   },
                   icon: Icon(Icons.logout_outlined),
                   label: Text(
